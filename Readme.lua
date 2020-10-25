8643d20b49f4c5a3ec6c62740c7009e0017ea705
sangoon <snkorp@gmail.com>
2020-10-25 10:53:44 +0100

    inital commit

---
+++ Readme.lua
@@ -0,0 +1,51 @@
+Traduction française de Cultist Simulator 1.0
+
+Elle ne concerne que le jeu de base pour le moment et est finalisée à 100%.
+Ceci est le fruit de plus d'une centaine d'heures de travail,
+
+Si vous appréciez mes efforts et souhaitez faire un don Paypal: paypal.me/sirburton
+
+Pour signaler des erreurs ou mauvaises traductions: sir_burton@hotmail.com
+
+-------------
+
+* Installation sous Windows, copier le repertoire Traduction dans:
+
+
+C:\Utilisateurs\...\AppData\LocalLow\Weather Factory\Cultist Simulator\mods
+
+(Il s'agit d'un dossier caché à faire apparaitre dans les options: onglet Affichage, Options, onglet Affichage et cocher afficher les fichiers, dossiers cachés puis valider)
+
+Ensuite copier le fichier Strings.csv dans:
+
+...:\Steam\steamapps\common\Cultist Simulator\cultistsimulator_Data\StreamingAssets\content
+
+
+* Installation sous Linux, copier le repertoire Traduction dans:
+
+/home/USERNAME/.config/unity3d/Weather Factory/Cultist Simulator/mods/
+
+
+Ensuite copier le fichier Strings.csv dans:
+
+...:\Steam\steamapps\common\Cultist Simulator\cultistsimulator_Data\StreamingAssets\content
+
+
+* Installation sous Mac, copier le repertoire Traduction dans:
+
+Bibliothèque ▸ Application Support ▸ biz.weatherfactory.cultistsimulator ▸ Mods
+
+Ensuite copier le fichier Strings.csv dans:
+
+Bibliothèque ▸ Application Support ▸ Steam ▸ SteamApps ▸ common ▸ Cultist Simulator ▸ Content
+
+
+-------------
+
+Lancer le jeu et activer le mod dans l'onglet DLC en haut à droite
+Important, il est necessaire de relancer le jeu apres avoir chargé la traduction
+
+
+Enjoy
+
+Burton
